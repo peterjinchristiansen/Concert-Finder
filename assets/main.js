f082ba56ba8b38e499072e2b42960ec993650de9
Kelley Worthington <kelleymarne@gmail.com>
2021-06-08 19:54:11 -0600

    fixed youtube link issue, as well as clearing the previous search values

--- assets/main.js
+++ assets/main.js
@@ -10,7 +10,11 @@ var video = ''
 $('#find').on('click', function(event) {
     var search = $('#search').val()
     event.preventDefault()
-    
+
+    // clears out the values of previous search
+    $('#player').empty()
+    $('#event').empty()
+
     fetch(BTapiUrl + search + BTapiKey)
     .then(data => data.json())
     .then(result => {
@@ -92,7 +96,7 @@ $('#find').on('click', function(event) {
         result.items.forEach(item => {
             video = 
             `
-            <iframe width="480" height="375" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
+            <iframe width="480" height="375" src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
             `
             $('#player').append(video);
         })
